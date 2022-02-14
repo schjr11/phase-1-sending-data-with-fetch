243f367793589ee884ead82d8a8d0579feaa21dc
Scott Hill <scott.c.hill.jr@gmail.com>
2022-02-14 14:45:18 -0600

    commit tree b56c727a62b477623ec5c1890287fee181184aaa

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(inputName, inputEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: inputName,
+            email: inputEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        const newItemId = json.id;
+        const p = document.createElement('p')
+        p.textContent = newItemId;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+    .catch(someError => {
+        const p = document.createElement('p')
+        p.textContent = someError.message;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+}
